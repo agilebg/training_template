726d7224fe4265357b57711b0f121cf5c7424605
hurrinico <nicola.malcontenti@agilebg.com>
2018-02-08 17:09:29 +0100

    [FIX] add website

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -14,19 +14,19 @@
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'website', 'website_sale'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        # 'data.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
